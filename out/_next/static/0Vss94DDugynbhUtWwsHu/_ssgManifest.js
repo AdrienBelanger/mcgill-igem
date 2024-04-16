568ce2c8e357536e6727f63693a5f48e737d8146
Adrien.gmail <adribelanger@gmail.com>
2024-04-16 08:54:44 -0400

    added new deploy

--- out/_next/static/0Vss94DDugynbhUtWwsHu/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fjoin","\u002Fcollaborators","\u002F2022","\u002Fnotfound","\u002Fmission","\u002Fpartner","\u002Fcontact","\u002Fabout","\u002Fsponsors","\u002Fartcontest","\u002Fcurrent"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ out/_next/static/mlSrqL5W033iblKzSZxMw/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcollaborators","\u002F","\u002Fjoin","\u002Fcurrent","\u002Fnotfound","\u002Fpartner","\u002Fmission","\u002Fabout","\u002Fsponsors","\u002F2022","\u002Fartcontest","\u002Fcontact"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
