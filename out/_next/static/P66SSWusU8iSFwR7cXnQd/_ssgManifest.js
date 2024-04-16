dba6c4703be27dd1b3c1083f23d23933f0e16dfb
Adrien.gmail <adribelanger@gmail.com>
2024-04-16 10:09:56 -0400

    corrected navbar

--- out/_next/static/P66SSWusU8iSFwR7cXnQd/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F2022","\u002Fcontact","\u002F","\u002Fnotfound","\u002Fmission","\u002Fpartner","\u002Fcurrent","\u002Fabout","\u002Fjoin","\u002Fsponsors","\u002Fartcontest","\u002Fcollaborators"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
