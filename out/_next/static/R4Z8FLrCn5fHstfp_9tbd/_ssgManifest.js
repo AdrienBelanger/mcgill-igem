a5e9f5efb839411a771faff00f841461749d8b8f
AdrienBelanger <adribelanger@gmail.com>
2024-05-01 10:37:52 -0400

    Correctly updated static files

---
+++ out/_next/static/R4Z8FLrCn5fHstfp_9tbd/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fartcontest","\u002F","\u002Fjoin","\u002Fmission","\u002Fnotfound","\u002Fpartner","\u002F2022","\u002Fcollaborators","\u002Fsponsors","\u002Fabout","\u002Fcurrent","\u002Fcontact"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
