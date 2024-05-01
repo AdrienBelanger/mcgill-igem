e5a46ff4373fb53f13008ab790bdabe5b08fa4ce
AdrienBelanger <adribelanger@gmail.com>
2024-05-01 17:33:02 -0400

    Fixed Contact Page

--- out/_next/static/arf1YFtLn5zlpu0Q8C8wr/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcontact","\u002Fjoin","\u002F","\u002Fmission","\u002Fnotfound","\u002Fpartner","\u002Fabout","\u002Fsponsors","\u002Fcollaborators","\u002F2022","\u002Fcurrent","\u002Fartcontest"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ out/_next/static/oZZKfxA8z9f77pWwzkxjL/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fmission","\u002Fjoin","\u002Fnotfound","\u002Fcurrent","\u002Fpartner","\u002Fsponsors","\u002Fabout","\u002F2022","\u002Fcollaborators","\u002Fartcontest"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
