410133fbeb6d3dcffe6cf71b50916e89db98ef53
AdrienBelanger <adribelanger@gmail.com>
2024-05-01 17:17:05 -0400

    Updated static with last commit

--- out/_next/static/fGP2wBCIBgGDLXMxDeSLF/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F2022","\u002F","\u002Fjoin","\u002Fmission","\u002Fnotfound","\u002Fpartner","\u002Fartcontest","\u002Fabout","\u002Fsponsors","\u002Fcurrent","\u002Fcollaborators","\u002Fcontact"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
