ac21ba6f536569b61600ce704e3ba7810e697d86
AdrienBelanger <adribelanger@gmail.com>
2024-05-01 11:22:31 -0400

    Updated Artcontext.tsx and fixed deps

---
+++ out/_next/static/OQ-E_tLeo7dHHy7umZtae/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcurrent","\u002F","\u002Fjoin","\u002Fmission","\u002F2022","\u002Fpartner","\u002Fnotfound","\u002Fartcontest","\u002Fabout","\u002Fsponsors","\u002Fcontact","\u002Fcollaborators"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- pages/artcontest.tsx
+++ pages/artcontest.tsx
@@ -32,84 +32,82 @@ const artcontest: NextPage = () => {
         </div>
 
         <div className="py-10 pl-40">
-          <img src="img/Synbiosis Art competition - Projet Casa display.svg" alt="Image of the Art Gallery" width={1200} height={800} />
+          <img src="img/Synbiosis Art competition - Projet Casa display.svg" alt="Image of the Art Gallery" style={{width: '90%'}} />
         </div>
   
+
+
+
+
         <div className="py-10 pl-40">
           <div className="w-1/2 font-signika font-regular text-5xl">
             Description
           </div>
 
           <div className="w-1/2 mt-14 font-mono font-regular text-l">
-
-
-
-          <div>
-            <b className="w-1/2 mt-14 font-mono font-regular text-xl">Who is this competition for?</b>
-            <br/>
-            For anyone who has a kick for biology and biotech in general. But it applies just as well to anyone who is up to participate and take part of an exciting project that merges art with synthetic biology.
+            <div>
+              <b className="w-1/2 mt-14 font-mono font-regular text-xl">Who is this competition for?</b>
+              <br/>
+              For anyone who has a kick for biology and biotech in general. But it applies just as well to anyone who is up to participate and take part of an exciting project that merges art with synthetic biology.
+            </div>
+            <br /><br />
+            <b className="w-1/2 mt-14 font-mono font-regular text-xl">What is synthetic biology (or synbio)?</b>
+            <br />
+            Synthetic biology is a new field of research with a central focus on how DNA can be engineered as a tool to create new systems that have a purpose, whether it be from DNA origami biosensors to smart bacteria that can reach tumor sites and release therapeutics. The limit is your imagination...
+            <br /><br />
+            To get a better grasp of what synbio is, feel free to check out <a href="https://www.youtube.com/watch?v=zggU1KCgc7A" className="text-blue-500 hover:underline">this explanatory video</a> by Ginkgo Bioworks, a world leader in the field.
+          
           </div>
-  What is synthetic biology (or synbio)?
-  <br />
-  Synthetic biology is a new field of research with a central focus on how DNA can be engineered as a tool to create new systems that have a purpose, whether it be from DNA origami biosensors to smart bacteria that can reach tumor sites and release therapeutics. The limit is your imagination...
-</div>
 
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  To get a better grasp of what synbio is, feel free to check out <a href="https://www.youtube.com/watch?v=zggU1KCgc7A" className="text-blue-500 hover:underline">this explanotory video</a> by Ginkgo Bioworks, a world leader in the field.
-</div>
-
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  Our theme
-  <br />
-  Synbio, Synbiosis - With engineering and the art of biology, at the micro-scale
-  <br />
-  The idea behind is to show synbio as a way to harness nature’s amazing potential as a framework for finding new solutions to help benefit society as a whole. This involves a synergy between creativity, knowledge of current tools, and goodwill, to make it work. Just as science and art are both powered by creativity and curiosity, we hope that you will be inspired by science, and in turn, inspire future scientists.
-</div>
-
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  Need inspiration?
-  <br />
-  ...
-</div>
-
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  Rules
-  <br />
-  Anything you can paint on a canvas or digitally will be accepted. You can also explore other mediums (such as using clay). Make sure you follow the theme, and the message we try to convey with it. The work you submit must be your own - we reserve the right to disqualify your artwork if it has copied, or plagiarized any other work without explicit permission.
-</div>
-
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  How to submit?
-  <br />
-  ...
-  
-</div>
-
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  How many people can win?
-  <br />
-  ...
-
-</div>
-
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  Key Dates
-  <br />
-  Submission deadline - June 28 2024
-  <br />
-  Tentative date of final decisions - July 5 2024
-  <br />
-  Exhibition - July 18-21 2024
-</div>
-
-<div className="w-1/2 mt-14 font-mono font-regular text-l">
-  Questions?
-  <br />
-  If you have any questions or want some pointers on how you can pursue your work, do not hesitate to contact us to our email at igem@mcgill.ca
-</div>
-
+          <div className="w-1/2 mt-14 font-mono font-regular text-l">
+            Our theme
+            <br />
+            Synbio, Synbiosis - With engineering and the art of biology, at the micro-scale
+            <br /><br />
+            The idea behind is to show synbio as a way to harness nature’s amazing potential as a framework for finding new solutions to help benefit society as a whole. This involves a synergy between creativity, knowledge of current tools, and goodwill, to make it work. Just as science and art are both powered by creativity and curiosity, we hope that you will be inspired by science, and in turn, inspire future scientists.
           </div>
+
+          <div className="w-1/2 mt-14 font-mono font-regular text-l">
+            Need inspiration?
+            <br />
+            ...
           </div>
+
+          <div className="w-1/2 mt-14 font-mono font-regular text-l">
+            Rules
+            <br />
+            Anything you can paint on a canvas or digitally will be accepted. You can also explore other mediums (such as using clay). Make sure you follow the theme, and the message we try to convey with it. The work you submit must be your own - we reserve the right to disqualify your artwork if it has copied, or plagiarized any other work without explicit permission.
+          </div>
+
+          <div className="w-1/2 mt-14 font-mono font-regular text-l">
+            How to submit?
+            <br />
+            ...
+          </div>
+
+          <div className="w-1/2 mt-14 font-mono font-regular text-l">
+            How many people can win?
+            <br />
+            ...
+          </div>
+
+          <div className="w-1/2 mt-14 font-mono font-regular text-l">
+            Key Dates
+            <br />
+            Submission deadline - June 28 2024
+            <br />
+            Tentative date of final decisions - July 5 2024
+            <br />
+            Exhibition - July 18-21 2024
+          </div>
+
+          <div className="w-1/2 mt-14 font-mono font-regular text-l">
+            Questions?
+            <br />
+            If you have any questions or want some pointers on how you can pursue your work, do not hesitate to contact us to our email at igem@mcgill.ca
+          </div>
+        </div>
+      </div>
     </>
   );
 };
