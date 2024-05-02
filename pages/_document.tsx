6df90ddf4ba3415175c3426889b363c79babc000
AdrienBelanger <adribelanger@gmail.com>
2024-05-02 09:29:03 -0400

    Updated to ensure no quirks mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default function MyDocument() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Ibarra+Real+Nova&family=Monoton&family=Montserrat:wght@400;700;900&family=Signika:wght@300;400;500;600;700&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Ibarra+Real+Nova&family=Monoton&family=Montserrat:wght@400;700;900&family=Signika:wght@300;400;500;600;700&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        {/* Other meta tags, title, and links */}
       </Head>
       <body>
         <Main />
@@ -15,5 +18,3 @@ export default function Document() {
     </Html>
   );
 }
-
-
